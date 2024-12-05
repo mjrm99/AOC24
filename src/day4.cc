a59c169ff0b7399b4649948a5300a8e54d66ae74
Manuel José Reyes Molinari <mjreyesm99@gmail.com>
2024-12-04 23:10:12 -0300

    feat: Day 4 part 1

--- src/day4.cc
+++ src/day4.cc
@@ -1,6 +1,37 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 #include "days.h"
 
-#include <fstream>
+struct Coord {
+  int x;
+  int y;
+};
+
+constexpr std::array<Coord, 8> directions = {Coord{1, 0}, Coord{1, 1}, Coord{0, 1}, Coord{-1, 1}, Coord{-1, 0}, Coord{-1, -1}, Coord{0, -1}, Coord{1, -1}};
+
+int CheckDirection(std::vector<std::string> grid, Coord coord, int direction_index, std::string pattern) {
+  Coord direction = directions[direction_index];
+  int minLength = (ssize(pattern) - 1);
+
+  Coord final_coord = {coord.x + minLength * direction.x, coord.y + minLength * direction.y};
+
+  if (final_coord.x >= 0 && final_coord.x < ssize(grid[0])) {
+    if (final_coord.y >= 0 && final_coord.y < ssize(grid)) {
+      for (int i = 1; i < ssize(pattern); i++) {
+        Coord current_coord = {coord.x + i * direction.x, coord.y + i * direction.y};
+        if (grid[current_coord.y][current_coord.x] != pattern[i]) {
+          return 0;
+        }
+      }
+      return 1;
+    }
+  }
+
+  return 0;
+}
 
 int SolveDay4(int part, bool is_example) {
   int result = 0;
@@ -8,10 +39,25 @@ int SolveDay4(int part, bool is_example) {
   std::filesystem::path input_path = GetInputPath(4, part, is_example);
   std::ifstream input_file(input_path);
   if (input_file.is_open()) {
-    std::string line;
+    std::vector<std::string> lines;
+    const std::string pattern = "XMAS";
+    const char start_char = pattern[0];
 
+    std::string line;
     while (std::getline(input_file, line)) {
-      std::string current_line = line;
+      lines.emplace_back(line);
+    }
+
+    for (int y = 0; y < ssize(lines); y++) {
+      std::string current_line = lines[y];
+      size_t x = current_line.find(start_char); 
+      while (x != current_line.npos) {
+        current_line.replace(x, 1, 1, '.');
+        for (int i = 0; i < ssize(directions); i++) {
+          result += CheckDirection(lines, Coord(x, y), i, pattern);
+        }
+        x = current_line.find(start_char);
+      }
     }
   }
 
